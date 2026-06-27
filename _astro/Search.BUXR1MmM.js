87a302c040f60ac5279c73db3029cbebf3e219be
xn <pengfei20140701@126.com>
2026-06-27 16:29:04 +0800

    feat(deploy): production build with global getStaticPaths fix for all dynamic routes

--- _astro/Search.BUXR1MmM.js
+++
@@ -1,1 +0,0 @@
-import{default as i}from"./Search.CE6tKQbu.js";import"./jsx-runtime.ZsPXNmrQ.js";import"./index.Bs6cAnji.js";import"./index.Bza6HD7l.js";export{i as default};
---
+++ _astro/Search.DiiJE8xj.js
@@ -0,0 +1,1 @@
+import{default as i}from"./Search.DLpauW5u.js";import"./jsx-runtime.Bn8z3z5i.js";import"./vendor-charts.DDAwSU94.js";import"./index.CivqDGqC.js";export{i as default};
