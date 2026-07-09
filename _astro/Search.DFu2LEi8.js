bab019fefad7c3d5527fad73eca531de1d88cffc
xn <pengfei20140701@126.com>
2026-07-09 10:32:56 +0800

    performance(search): 重构 BaseLayout 懒加载 SearchBoot 提升 HTML 首屏性能

--- _astro/Search.DFu2LEi8.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"./Search.H3igUAVD.js";import"./preload-helper.BlTxHScW.js";import"./jsx-runtime.CyXy1Ci3.js";import"./index.CaZlGE7t.js";import"./index.DGV8gdK3.js";export{e as default};
---
+++ _astro/Search.Doq6O8uw.js
@@ -0,0 +1,1 @@
+import{default as e}from"./Search.CgbCrOln.js";import"./preload-helper.BlTxHScW.js";import"./jsx-runtime.CyXy1Ci3.js";import"./index.CaZlGE7t.js";import"./index.uNcHzTdQ.js";export{e as default};
